29dd0661a048e1cf3f3ca2d4326bf56fbc1c5dcc
LoreDreamer <55300635+LoreDreamer@users.noreply.github.com>
2024-08-23 15:52:29 +0000

    Conceptualización Persona
    Se crea clase persona con sus componentes básicos.

--- src/main/java/Persona.java
+++ src/main/java/Persona.java
@@ -2,6 +2,42 @@ public class Persona {
 
   private String nombre;
   private int rut;
-  private boolean esExpositor = false;
+  private boolean esExpositor;
+
+  public Persona(String nombre, int rut, boolean esExpositor) {
+    this.nombre = nombre;
+    this.rut = rut;
+    this.esExpositor = esExpositor;
+  }
+
+  public String getNombre() {
+    return nombre;
+  }
+
+  public void setNombre(String nombre) {
+    this.nombre = nombre;
+  }
+
+  public int getRut() {
+    return rut;
+  }
+
+  public void setRut(int rut) {
+    this.rut = rut;
+  }
+
+  public boolean isEsExpositor() {
+    return esExpositor;
+  }
+
+  public void setEsExpositor(boolean esExpositor) {
+    this.esExpositor = esExpositor;
+  }
+
+  public void mostrarInformacion() {
+    System.out.println("Nombre: " + nombre);
+    System.out.println("Rut: " + rut);
+    System.out.println("Expositor: " + esExpositor);
+  }
   
 }
